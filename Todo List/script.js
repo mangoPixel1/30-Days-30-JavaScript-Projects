7a438b865b2de020fe7420173f5ed1983453e445
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2024-03-08 12:04:09 -0700

    Mark items as complete

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,20 +1,34 @@
+// DONE: How to access grandparent of an element and edit a node with no HTML tag
+// ^^^ write it using an event handler
+// What are some ways I can normalize a CSS file to ensure prevent cross-browser issues?
+// How do I use DOMContentLoaded, give me an example
+// Refactor code to use functions and instead of having function logic in the body of addEventListener()
+
+
 // HTML tag references
 const textInputBox = document.getElementById('newTaskInput'); // New task input
 const addBtn = document.getElementById('addBtn'); // Add button
 const tasksList = document.getElementById('tasks'); // Tasks list
 
-function addNewTask() {
+
+// Handle adding new task to list when 'Add' button is clicked
+addBtn.addEventListener('click', () => {
     // Check if text box contains text
     if (textInputBox.value.length == 0) {
         alert('Text box is empty.')
         return;
     } else {
-        // Create new task item div
+        // Create new task item <div>
         const newTask = document.createElement('div');
         newTask.classList.add('taskItem');
-        newTask.textContent = textInputBox.value;
+        
+        // Create new task text <p>
+        const taskText = document.createElement('p');
+        taskText.classList.add('taskText');
+        taskText.textContent = textInputBox.value;
+        newTask.appendChild(taskText);
 
-        // Create new taskOptions div (Complete & Delete)
+        // Create new taskOptions <div> (Complete & Delete)
         const taskOptions = document.createElement('div');
         taskOptions.classList.add('taskOptions');
         
@@ -37,22 +51,34 @@ function addNewTask() {
         // Reset input text box
         textInputBox.value = '';
     }
-}
+});
 
-// Handle adding new task to list when 'Add' button is clicked
-addBtn.addEventListener('click', addNewTask);
-
-// Mark items as completed/deleted when Complete/Delete button is clicked
+// Handle marking a task as complete when Complete button is clicked
 tasksList.addEventListener('click', (event) => {
     const buttonClicked = event.target;
-    if (buttonClicked.matches('button')) {
-        if (buttonClicked.classList.contains('completeBtn')) {
-            // Access parent element
-            //buttonClicked.classList.toggle('completed');
-            alert('Task completed')
-        } else if (buttonClicked.classList.contains('deleteBtn')) {
-            alert('Task deleted');
+    if (buttonClicked.matches('button') && buttonClicked.classList.contains('completeBtn')) {
+        const taskText = event.target.parentElement.parentElement.firstElementChild;
+
+        // Toggle the line-through style 
+        if (taskText.style.textDecoration === "line-through") {
+            taskText.style.textDecoration = "none";
+        } else {
+            taskText.style.textDecoration = "line-through";
         }
-        
-    } 
+    }
+});
+
+// Handle deleting a task when Delete button is clicked
+tasksList.addEventListener('click', (event) => {
+    const buttonClicked = event.target;
+    if (buttonClicked.matches('button') && buttonClicked.classList.contains('completeBtn')) {
+        const taskText = event.target.parentElement.parentElement.firstElementChild;
+
+        // Toggle the line-through style 
+        if (taskText.style.textDecoration === "line-through") {
+            taskText.style.textDecoration = "none";
+        } else {
+            taskText.style.textDecoration = "line-through";
+        }
+    }
 });
